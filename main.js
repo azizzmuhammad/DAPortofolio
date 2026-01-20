02ac15ddfa316adc3cb868f938f6c0e3ff29c952
azizzmuhammad <azizzmuhamad44@gmail.com>
2026-01-20 13:35:26 +0700

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,49 @@
 // Simple form submit handler
-document.querySelector(".contact-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Thank you! Your message has been sent.");
-    this.reset();
-  });
+const form = document.getElementById("contactForm");
+const statusText = document.getElementById("status");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  statusText.textContent = "Sending...";
+  statusText.style.color = "#2563eb";
+
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwmWmNHSVa8qpVLhbFWBpD5pmk98vQUkOysoHYNOGVcPZQsyK8njzHc790gCxzDNa8/exec", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+      
+
+    const result = await response.json();
+
+    if (result.status === "success") {
+      statusText.textContent = "Message sent successfully!";
+      statusText.style.color = "green";
+      form.reset();
+    } else {
+      throw new Error();
+    }
+  } catch {
+    statusText.textContent = "Failed to send message.";
+    statusText.style.color = "red";
+  }
+});
+
+// document.querySelector(".contact-form").addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     alert("Thank you! Your message has been sent.");
+//     this.reset();
+//   });
 /* HAMBURGER MENU */
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
@@ -33,3 +73,10 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+// Smooth scroll & close mobile menu
+document.querySelectorAll('.nav-btn').forEach(link => {
+    link.addEventListener('click', () => {
+      navMenu.classList.remove('show');
+    });
+  });
